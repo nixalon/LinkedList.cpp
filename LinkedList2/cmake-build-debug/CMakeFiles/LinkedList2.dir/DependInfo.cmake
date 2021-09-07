f71a6384e81f83b8dd679adf89d5153b14a09b8e
nixalon <47711662+nixalon@users.noreply.github.com>
2021-09-07 17:52:47 +0200

    .gitignore

--- LinkedList2/cmake-build-debug/CMakeFiles/LinkedList2.dir/DependInfo.cmake
+++ LinkedList2/cmake-build-debug/CMakeFiles/LinkedList2.dir/DependInfo.cmake
@@ -8,7 +8,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/Alexandra/CLionProjects/LinkedList2/main.cpp" "C:/Users/Alexandra/CLionProjects/LinkedList2/cmake-build-debug/CMakeFiles/LinkedList2.dir/main.cpp.obj"
+  "C:/Users/Alexandra/Documents/GitHub/LinkedList.cpp/LinkedList2/main.cpp" "C:/Users/Alexandra/Documents/GitHub/LinkedList.cpp/LinkedList2/cmake-build-debug/CMakeFiles/LinkedList2.dir/main.cpp.obj"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- LinkedList2/cmake-build-debug/cmake_install.cmake
+++ LinkedList2/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Alexandra/CLionProjects/LinkedList2
+# Install script for directory: C:/Users/Alexandra/Documents/GitHub/LinkedList.cpp/LinkedList2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Alexandra/CLionProjects/LinkedList2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Alexandra/Documents/GitHub/LinkedList.cpp/LinkedList2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
